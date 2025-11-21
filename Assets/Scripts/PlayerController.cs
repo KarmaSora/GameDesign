861f3cc32b33351b6627cf39a12dd660d479f253
Karam Matar <karammtr@gmail.com>
2025-11-21 14:35:17 +0000

    player jumps only once on platform, can move sideways

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,29 +8,59 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody playerRigidBody;
     public float playerSPeed = 10.0f;
-    public float playerJumpForce = 5.0f;
+    public float playerJumpForce = 25.0f;
 
     public GameObject focalPoint;
+
+    public bool isGrounded;
+    public float gravityModifier = 1;
 
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("FocalPoint");
+        Physics.gravity *= gravityModifier;
 
 
     }
 
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+    
     // Update is called once per frame
     void Update()
     {
         float veritcalInput = Input.GetAxis("Vertical");
 
-        playerRigidBody.AddForce(focalPoint.transform.forward * veritcalInput * playerSPeed);
+        float horizontal = Input.GetAxis("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        //playerRigidBody.AddForce(focalPoint.transform.forward * veritcalInput * playerSPeed);
+        //playerRigidBody.AddForce(focalPoint.transform.right * horizontal * playerSPeed);
+        playerRigidBody.transform.Translate(Vector3.forward * veritcalInput * Time.deltaTime * playerSPeed );
+        playerRigidBody.transform.Translate(Vector3.right * horizontal * Time.deltaTime * playerSPeed );
+
+
+        if (Input.GetKeyDown(KeyCode.Space)  && isGrounded)
         {
             playerRigidBody.AddForce(Vector3.up * playerJumpForce, ForceMode.Impulse);
+
+            isGrounded = false;
         }
 
+
     }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+
 }
